32f23800ba49460ed3511b693c0272718cded191
Vanshika190 <vanshikagahlot561@gmail.com>
2024-06-27 21:41:53 +0530

    Popular in women

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Hero from '../components/Hero/Hero'
+import Popular from '../components/Popular/Popular'
+
 
 const shop = () => {
   return (
     <div>
+      <Hero/>
+      <Popular/>
       
     </div>
   )
